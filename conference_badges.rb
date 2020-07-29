4aeac11b7e44bd1da584f17f36c668875d9ede2d
Valentina E <tiyahelfrih@gmail.com>
2020-07-29 12:13:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,6 @@ people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  return 
