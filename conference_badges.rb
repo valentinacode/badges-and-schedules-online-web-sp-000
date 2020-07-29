6fc24168e17432afde3482a6c5794b4af58f4f8b
Valentina E <tiyahelfrih@gmail.com>
2020-07-29 12:16:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,3 +20,13 @@ def assign_rooms(array)
   end
   return nuarray
 end
+
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end 
+  
+  assign_rooms(array).each do |id|
+    puts id 
+  end
+end
