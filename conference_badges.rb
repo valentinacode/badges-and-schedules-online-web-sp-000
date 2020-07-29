c5a99c773783e66b76308b409366e2be91e25fa8
Valentina E <tiyahelfrih@gmail.com>
2020-07-29 12:14:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,10 +4,3 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
-  new_array = []
-  array.each do |name|
-    new_array.push("Hello, my name is #{name}.")
-  end
-  return new_array
-end
